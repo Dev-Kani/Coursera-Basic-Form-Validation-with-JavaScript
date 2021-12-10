e6bc2ff69cf1baad557fb7bb7eb882176ff97d4b
Kanishka Samarathunga <kanishkakcs@gmail.com>
2021-12-10 14:13:54 +0530

    completed

--- scripts.js
+++ scripts.js
@@ -2,7 +2,29 @@ function nameValidator() {
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
 
-    if (fname === '') {
-        console.log('fname');
+    
+    if (fname === "" && lname === "") {
+        alert("Full name is required")
+        return false;
+    }
+    if (fname === "" || lname === "") {
+        alert("Full name is required")
+        return false;
+    }
+};
+
+function emailValidator() {
+    let email = document.getElementById("email").value;
+    let regx = /^([a-z0-9\.-]+)@([a-z0-9]+).([a-z]{2,8})(.[a-z]{2,8})$/;
+
+
+    if(regx.test(email)) {
+        document.getElementById("invalidEmail").innerHTML = "Valid"
+        document.getElementById("invalidEmail").style.visibility = "visible"
+        document.getElementById("invalidEmail").style.color = "Green"
+    } else {
+        document.getElementById("invalidEmail").innerHTML = "Invalid"
+        document.getElementById("invalidEmail").style.visibility = "visible"
+        document.getElementById("invalidEmail").style.color = "Red"
     }
 }
