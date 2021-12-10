b162770933d0c6091df71a38303dbf99ef0c736a
Kanishka Samarathunga <kanishkakcs@gmail.com>
2021-12-10 07:57:09 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function nameValidator() {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+
+    if (fname === '') {
+        console.log('fname');
+    }
+}
